1773857d73452ebd0ef525c6431ab83652b24256
ssaech <101516958+ssaech@users.noreply.github.com>
2022-08-15 09:59:20 -0700

    Update mongooseSchemas.js

--- client/mongoDB/mongooseSchemas.js
+++ client/mongoDB/mongooseSchemas.js
@@ -7,29 +7,16 @@ const mongoTicketSchema = new mongoose.Schema({
     priority: Number,
     type: String,
     product: String,
-    browser: String,
     screenshot:{ type:  String, default: "None" },
     screenshotType: {type : String, default: "None"},
     summary: String,
-    reproductionSteps: String,
-    expectedResult: String,
-    actualResult: String,
     createdAt: { type: Date, default: Date.now },
     createdBy: String,
     updatedAt: { type: Date, default: Date.now },
     updatedBy: String,
-    assignedTo: String,
     status: String
 });
 
-const mongoMessageSchema = new mongoose.Schema({
-    chatId: Number,
-    subject: String,
-    message: String,
-    sender: String,
-    receiver: String,
-    sentAt: { type: Date, default: Date.now },
-})
+
 
 module.exports.mongoTicketSchema = mongoTicketSchema;
-module.exports.mongoMessageSchema = mongoMessageSchema;
