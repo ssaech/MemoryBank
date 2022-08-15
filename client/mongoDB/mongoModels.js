248118e3c761b771b2a71b37f2417a2ba0d12958
ssaech <101516958+ssaech@users.noreply.github.com>
2022-08-15 09:59:42 -0700

    Update mongoModels.js

--- client/mongoDB/mongoModels.js
+++ client/mongoDB/mongoModels.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const {mongoMessageSchema, mongoTicketSchema} = require("./mongooseSchemas");
 
 const Ticket = mongoose.model('Ticket', mongoTicketSchema);
-const Message = mongoose.model('Message', mongoMessageSchema);
+
 
 module.exports.Ticket = Ticket;
 module.exports.Message = Message;
